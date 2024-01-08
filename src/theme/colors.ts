38816c94823b7e2938a7a7ce79eb3073534ad0c2
Paulo Mendes <paulo.h.mendes25@gmail.com>
2024-01-08 00:25:12 -0300

    feat: patterns and theme configuration

---
+++ src/theme/colors.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  brand: {
+    primary: '#2F9638',
+  },
+  bg: {
+    main: '#010202',
+  },
+  text: {
+    primary: '#FFF',
+    secondary: '#F2F2F2',
+  },
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+const colors = require('./src/theme/colors');
 
 const config: Config = {
   content: [
@@ -7,14 +8,19 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: colors,
     extend: {
+      fontFamily: {
+        montserrat: ['var(--font-montserrat)'],
+        rubik: ['var(--font-rubik)'],
+      },
       backgroundImage: {
+        'green-gradient': 'linear-gradient(78deg, #1E8327 0%, #85BC8A 99.48%)',
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
