fd6b738718969b919b97f21166062eb89ff444c0
Paulo Mendes <paulo.h.mendes25@gmail.com>
2024-01-09 23:08:40 -0300

    feat: tired of analysis textual content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { GreenRange } from '@/layout/GreenRange';
 import Hero from '@/layout/Hero';
+import { TiredOfAnalysis } from '@/layout/TiredOfAnalysis';
 
 export default function Home() {
   return (
     <main className="flex flex-col text-text-primary">
       <Hero.Desktop />
       <GreenRange />
+      <TiredOfAnalysis />
     </main>
   );
 }
