4a7c77ab4a6d0ecc5faeecdd1ab735d640a32e84
Paulo Mendes <paulo.h.mendes25@gmail.com>
2024-01-16 00:03:50 -0300

    feat: finishing mobile version

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
   return (
-    <footer className="w-full py-[80px] bg-green-gradient flex justify-center items-center">
+    <footer className="w-full py-[51px] md:py-[80px] bg-green-gradient flex justify-center items-center">
       <span className="text-bg-main text-sm font-normal">
-        © 2024 betstats TODOS OS DIREITOS RESERVADOS | contato@betstats.online
+        © 2024 betstats TODOS OS DIREITOS RESERVADOS
       </span>
     </footer>
   );
